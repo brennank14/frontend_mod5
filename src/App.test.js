a72d9195fbe5a593e91e8789a2578cf8ef47e00e
brennank14 <brennank14@gmail.com>
2020-09-15 12:50:07 -0500

    app and index setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path='/login' component={LoginContainer} />
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
